cfbe021e178e9bf474035a4fcc447fc67c91c30e
2400031662 <settysivatejaswi@gmail.com>
2025-11-04 23:01:11 +0530

    Add simple-interest.sh script for interest calculation
    This script calculates simple interest based on principal, rate, and time inputs.

---
+++ simple-interest.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# simple-interest.sh : Script to calculate simple interest
+
+# Check if three arguments are passed
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <principal> <rate> <time>"
+    exit 1
+fi
+
+principal=$1
+rate=$2
+time=$3
+
+interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+echo "Simple Interest: $interest"
